3070d03141cc3e49ff3a1d9e6c0570017e6fa0a9
Sergey Molchanov <sergey.molchanov.25@gmail.com>
2012-11-14 01:47:41 +0200

    Added main functionality

--- WinFormsGameBalda/Form1.Designer.cs
+++ WinFormsGameBalda/Form1.Designer.cs
@@ -28,12 +28,318 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radGroupBoxPl1 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radTxtBxScorePl1 = new Telerik.WinControls.UI.RadTextBox();
+            this.pctrPl1 = new System.Windows.Forms.PictureBox();
+            this.lblScorePl1 = new Telerik.WinControls.UI.RadLabel();
+            this.radTxtBxNm1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radGroupBoxPl2 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radTxtBxPl2 = new Telerik.WinControls.UI.RadTextBox();
+            this.pctrPl2 = new System.Windows.Forms.PictureBox();
+            this.lblScorePl2 = new Telerik.WinControls.UI.RadLabel();
+            this.radTxtBxNm2 = new Telerik.WinControls.UI.RadTextBox();
+            this.dtgridView = new System.Windows.Forms.DataGridView();
+            this.aquaTheme1 = new Telerik.WinControls.Themes.AquaTheme();
+            this.radMenuItem2 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenu1 = new Telerik.WinControls.UI.RadMenu();
+            this.MnItmMenu = new Telerik.WinControls.UI.RadMenuItem();
+            this.MnItmHelp = new Telerik.WinControls.UI.RadMenuItem();
+            this.MnItmExit = new Telerik.WinControls.UI.RadMenuItem();
+            this.dtGrVwSymb = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBoxPl1)).BeginInit();
+            this.radGroupBoxPl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radTxtBxScorePl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrPl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblScorePl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTxtBxNm1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBoxPl2)).BeginInit();
+            this.radGroupBoxPl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radTxtBxPl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrPl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblScorePl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTxtBxNm2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radMenu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrVwSymb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(256, 333);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(144, 33);
+            this.radButton1.TabIndex = 0;
+            this.radButton1.Text = "Check the word";
+            this.radButton1.ThemeName = "Aqua";
+            // 
+            // radGroupBoxPl1
+            // 
+            this.radGroupBoxPl1.Controls.Add(this.radTxtBxScorePl1);
+            this.radGroupBoxPl1.Controls.Add(this.pctrPl1);
+            this.radGroupBoxPl1.Controls.Add(this.lblScorePl1);
+            this.radGroupBoxPl1.Controls.Add(this.radTxtBxNm1);
+            this.radGroupBoxPl1.FooterImageIndex = -1;
+            this.radGroupBoxPl1.FooterImageKey = "";
+            this.radGroupBoxPl1.FooterTextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radGroupBoxPl1.HeaderAlignment = Telerik.WinControls.UI.HeaderAlignment.Center;
+            this.radGroupBoxPl1.HeaderImageIndex = -1;
+            this.radGroupBoxPl1.HeaderImageKey = "";
+            this.radGroupBoxPl1.HeaderMargin = new System.Windows.Forms.Padding(0);
+            this.radGroupBoxPl1.HeaderText = "Player 1";
+            this.radGroupBoxPl1.HeaderTextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radGroupBoxPl1.Location = new System.Drawing.Point(12, 36);
+            this.radGroupBoxPl1.Name = "radGroupBoxPl1";
+            this.radGroupBoxPl1.Padding = new System.Windows.Forms.Padding(10, 20, 10, 10);
+            // 
+            // 
+            // 
+            this.radGroupBoxPl1.RootElement.Padding = new System.Windows.Forms.Padding(10, 20, 10, 10);
+            this.radGroupBoxPl1.Size = new System.Drawing.Size(200, 330);
+            this.radGroupBoxPl1.TabIndex = 2;
+            this.radGroupBoxPl1.Text = "Player 1";
+            this.radGroupBoxPl1.ThemeName = "Aqua";
+            // 
+            // radTxtBxScorePl1
+            // 
+            this.radTxtBxScorePl1.Location = new System.Drawing.Point(58, 257);
+            this.radTxtBxScorePl1.Name = "radTxtBxScorePl1";
+            this.radTxtBxScorePl1.ReadOnly = true;
+            this.radTxtBxScorePl1.Size = new System.Drawing.Size(129, 20);
+            this.radTxtBxScorePl1.TabIndex = 5;
+            this.radTxtBxScorePl1.TabStop = false;
+            this.radTxtBxScorePl1.Text = "Player Score";
+            this.radTxtBxScorePl1.ThemeName = "Aqua";
+            // 
+            // pctrPl1
+            // 
+            this.pctrPl1.Location = new System.Drawing.Point(13, 50);
+            this.pctrPl1.Name = "pctrPl1";
+            this.pctrPl1.Size = new System.Drawing.Size(174, 139);
+            this.pctrPl1.TabIndex = 4;
+            this.pctrPl1.TabStop = false;
+            // 
+            // lblScorePl1
+            // 
+            this.lblScorePl1.Location = new System.Drawing.Point(13, 260);
+            this.lblScorePl1.Name = "lblScorePl1";
+            this.lblScorePl1.Size = new System.Drawing.Size(39, 16);
+            this.lblScorePl1.TabIndex = 1;
+            this.lblScorePl1.Text = "Score:";
+            // 
+            // radTxtBxNm1
+            // 
+            this.radTxtBxNm1.AutoScroll = true;
+            this.radTxtBxNm1.Location = new System.Drawing.Point(13, 23);
+            this.radTxtBxNm1.Name = "radTxtBxNm1";
+            this.radTxtBxNm1.Size = new System.Drawing.Size(174, 20);
+            this.radTxtBxNm1.TabIndex = 0;
+            this.radTxtBxNm1.TabStop = false;
+            this.radTxtBxNm1.Text = "Player name";
+            this.radTxtBxNm1.ThemeName = "Aqua";
+            // 
+            // radGroupBoxPl2
+            // 
+            this.radGroupBoxPl2.Controls.Add(this.radTxtBxPl2);
+            this.radGroupBoxPl2.Controls.Add(this.pctrPl2);
+            this.radGroupBoxPl2.Controls.Add(this.lblScorePl2);
+            this.radGroupBoxPl2.Controls.Add(this.radTxtBxNm2);
+            this.radGroupBoxPl2.FooterImageIndex = -1;
+            this.radGroupBoxPl2.FooterImageKey = "";
+            this.radGroupBoxPl2.FooterTextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radGroupBoxPl2.HeaderAlignment = Telerik.WinControls.UI.HeaderAlignment.Center;
+            this.radGroupBoxPl2.HeaderImageIndex = -1;
+            this.radGroupBoxPl2.HeaderImageKey = "";
+            this.radGroupBoxPl2.HeaderMargin = new System.Windows.Forms.Padding(0);
+            this.radGroupBoxPl2.HeaderText = "Player 2";
+            this.radGroupBoxPl2.HeaderTextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radGroupBoxPl2.Location = new System.Drawing.Point(428, 36);
+            this.radGroupBoxPl2.Name = "radGroupBoxPl2";
+            this.radGroupBoxPl2.Padding = new System.Windows.Forms.Padding(10, 20, 10, 10);
+            // 
+            // 
+            // 
+            this.radGroupBoxPl2.RootElement.Padding = new System.Windows.Forms.Padding(10, 20, 10, 10);
+            this.radGroupBoxPl2.Size = new System.Drawing.Size(200, 330);
+            this.radGroupBoxPl2.TabIndex = 3;
+            this.radGroupBoxPl2.Text = "Player 2";
+            this.radGroupBoxPl2.ThemeName = "Aqua";
+            // 
+            // radTxtBxPl2
+            // 
+            this.radTxtBxPl2.Location = new System.Drawing.Point(71, 257);
+            this.radTxtBxPl2.Name = "radTxtBxPl2";
+            this.radTxtBxPl2.ReadOnly = true;
+            this.radTxtBxPl2.Size = new System.Drawing.Size(128, 20);
+            this.radTxtBxPl2.TabIndex = 6;
+            this.radTxtBxPl2.TabStop = false;
+            this.radTxtBxPl2.Text = "Player Score";
+            this.radTxtBxPl2.ThemeName = "Aqua";
+            // 
+            // pctrPl2
+            // 
+            this.pctrPl2.Location = new System.Drawing.Point(14, 50);
+            this.pctrPl2.Name = "pctrPl2";
+            this.pctrPl2.Size = new System.Drawing.Size(173, 139);
+            this.pctrPl2.TabIndex = 3;
+            this.pctrPl2.TabStop = false;
+            // 
+            // lblScorePl2
+            // 
+            this.lblScorePl2.Location = new System.Drawing.Point(14, 257);
+            this.lblScorePl2.Name = "lblScorePl2";
+            this.lblScorePl2.Size = new System.Drawing.Size(39, 16);
+            this.lblScorePl2.TabIndex = 2;
+            this.lblScorePl2.Text = "Score:";
+            // 
+            // radTxtBxNm2
+            // 
+            this.radTxtBxNm2.Location = new System.Drawing.Point(13, 23);
+            this.radTxtBxNm2.Name = "radTxtBxNm2";
+            this.radTxtBxNm2.Size = new System.Drawing.Size(174, 20);
+            this.radTxtBxNm2.TabIndex = 1;
+            this.radTxtBxNm2.TabStop = false;
+            this.radTxtBxNm2.Text = "Player name";
+            this.radTxtBxNm2.ThemeName = "Aqua";
+            // 
+            // dtgridView
+            // 
+            this.dtgridView.AllowDrop = true;
+            this.dtgridView.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dtgridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgridView.GridColor = System.Drawing.SystemColors.Control;
+            this.dtgridView.Location = new System.Drawing.Point(218, 36);
+            this.dtgridView.Name = "dtgridView";
+            this.dtgridView.RowHeadersWidth = 20;
+            this.dtgridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dtgridView.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dtgridView.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dtgridView.Size = new System.Drawing.Size(204, 165);
+            this.dtgridView.TabIndex = 4;
+            this.dtgridView.DragDrop += new System.Windows.Forms.DragEventHandler(this.dtgridView_DragDrop);
+            this.dtgridView.DragEnter += new System.Windows.Forms.DragEventHandler(this.dtgridView_DragEnter);
+            // 
+            // radMenuItem2
+            // 
+            this.radMenuItem2.Name = "radMenuItem2";
+            this.radMenuItem2.Text = "Help";
+            // 
+            // radMenu1
+            // 
+            this.radMenu1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.MnItmMenu,
+            this.MnItmHelp,
+            this.MnItmExit});
+            this.radMenu1.Location = new System.Drawing.Point(0, 0);
+            this.radMenu1.Name = "radMenu1";
+            this.radMenu1.Size = new System.Drawing.Size(638, 30);
+            this.radMenu1.TabIndex = 5;
+            this.radMenu1.Text = "radMenu1";
+            this.radMenu1.ThemeName = "Aqua";
+            // 
+            // MnItmMenu
+            // 
+            this.MnItmMenu.Name = "MnItmMenu";
+            this.MnItmMenu.Text = "Menu";
+            // 
+            // MnItmHelp
+            // 
+            this.MnItmHelp.Name = "MnItmHelp";
+            this.MnItmHelp.Text = "Help";
+            // 
+            // MnItmExit
+            // 
+            this.MnItmExit.Name = "MnItmExit";
+            this.MnItmExit.Text = "Exit";
+            this.MnItmExit.Click += new System.EventHandler(this.MnItmExit_Click);
+            // 
+            // dtGrVwSymb
+            // 
+            this.dtGrVwSymb.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dtGrVwSymb.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGrVwSymb.GridColor = System.Drawing.SystemColors.Control;
+            this.dtGrVwSymb.Location = new System.Drawing.Point(218, 207);
+            this.dtGrVwSymb.Name = "dtGrVwSymb";
+            this.dtGrVwSymb.RowHeadersWidth = 20;
+            this.dtGrVwSymb.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dtGrVwSymb.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dtGrVwSymb.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dtGrVwSymb.Size = new System.Drawing.Size(204, 45);
+            this.dtGrVwSymb.TabIndex = 6;
+            this.dtGrVwSymb.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dtGrVwSymb_CellMouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(638, 378);
+            this.ControlBox = false;
+            this.Controls.Add(this.dtGrVwSymb);
+            this.Controls.Add(this.radMenu1);
+            this.Controls.Add(this.dtgridView);
+            this.Controls.Add(this.radGroupBoxPl2);
+            this.Controls.Add(this.radGroupBoxPl1);
+            this.Controls.Add(this.radButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Molchanov, Teslia, Shevchenko";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBoxPl1)).EndInit();
+            this.radGroupBoxPl1.ResumeLayout(false);
+            this.radGroupBoxPl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radTxtBxScorePl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrPl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblScorePl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTxtBxNm1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBoxPl2)).EndInit();
+            this.radGroupBoxPl2.ResumeLayout(false);
+            this.radGroupBoxPl2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radTxtBxPl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrPl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblScorePl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTxtBxNm2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radMenu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrVwSymb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBoxPl1;
+        private Telerik.WinControls.UI.RadTextBox radTxtBxScorePl1;
+        private System.Windows.Forms.PictureBox pctrPl1;
+        private Telerik.WinControls.UI.RadLabel lblScorePl1;
+        private Telerik.WinControls.UI.RadTextBox radTxtBxNm1;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBoxPl2;
+        private Telerik.WinControls.UI.RadTextBox radTxtBxPl2;
+        private System.Windows.Forms.PictureBox pctrPl2;
+        private Telerik.WinControls.UI.RadLabel lblScorePl2;
+        private Telerik.WinControls.UI.RadTextBox radTxtBxNm2;
+        private System.Windows.Forms.DataGridView dtgridView;
+        private Telerik.WinControls.Themes.AquaTheme aquaTheme1;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem2;
+        private Telerik.WinControls.UI.RadMenu radMenu1;
+        private Telerik.WinControls.UI.RadMenuItem MnItmMenu;
+        private Telerik.WinControls.UI.RadMenuItem MnItmHelp;
+        private Telerik.WinControls.UI.RadMenuItem MnItmExit;
+        private System.Windows.Forms.DataGridView dtGrVwSymb;
     }
 }
 
--- WinFormsGameBalda/Form1.cs
+++ WinFormsGameBalda/Form1.cs
@@ -11,9 +11,155 @@ namespace WinFormsGameBalda
 {
     public partial class Form1 : Form
     {
+        private string[][] Massive = new string[5][];
+        private int[][] check = new int[2][];
+        private string[] symbols = new string[5];
         public Form1()
         {
+            for (int i = 0; i < check.Length; i++)
+            {
+                check[i] = new int[0];
+            }
             InitializeComponent();
+            InitializeMatrixProgramly();
+            DisplayMatrix(Massive, dtgridView);
+            for (int i = 0; i < Massive.Length; i++)
+                for (int j = 0; j < Massive[i].Length; j++)
+                    if (Massive[i][j] != "")
+                    {
+                        AddCheck(i, j);
+                    }
+            // Display Symbol Matrix
+            dtGrVwSymb.ColumnCount = symbols.Length;
+            dtGrVwSymb.Rows[0].Height = 30;
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                dtGrVwSymb.Columns[i].Width = 35;
+            }
+            dtGrVwSymb.Rows.Add(1);
+            for (int j = 0; j < symbols.Length; j++)
+                dtGrVwSymb.Rows[0].Cells[j].Value = symbols[j].ToString();
+        }
+        private void InitializeMatrixProgramly()
+        {
+            Massive[0] = new string[5] { "", "", "", "", "" };
+            Massive[1] = new string[5] { "", "", "", "", "" };
+            Massive[2] = new string[5] { "S","E","R","G","E" };
+            Massive[3] = new string[5] { "", "", "", "", "" };
+            Massive[4] = new string[5] { "", "", "", "", "" };
+            symbols = new string[5] { "A", "B", "C", "D", "E" };
+        }
+        /// <summary>
+        /// Display Matrix on component DataGridView.
+        /// </summary>
+        /// <param name="Massive">Matrix.</param>
+        /// <param name="dtgrdvw">DataGridView.</param>
+        public void DisplayMatrix(string[][] Massive, DataGridView dtgrdvw)
+        {
+            try
+            {
+                int max_col = Massive[0].Length;
+                for (int i = 0; i < Massive.Length; i++)
+                    if (Massive[i].Length > max_col)
+                        max_col = Massive[i].Length;
+                dtgrdvw.ColumnCount = 0;
+                dtgrdvw.ColumnCount = max_col;
+                dtgrdvw.Rows.Add(Massive.Length);
+                dtgrdvw.Visible = true;
+                for (int i = 0; i < max_col; i++)
+                {
+                    //dtgrdvw.Columns[i].Name = i.ToString();
+                    dtgridView.AutoSize = false;
+                    dtgrdvw.Columns[i].Width = 35;
+                    dtgridView.Rows[i].Height = 30;
+                    dtgrdvw.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                }
+                for (int i = 0; i < Massive.Length; i++)
+                    for (int j = 0; j < Massive[i].Length; j++)
+                        dtgrdvw.Rows[i].Cells[j].Value = Massive[i][j].ToString();
+            }
+            catch { MessageBox.Show("error"); }
+        }
+        private void UpdateFont()
+        {
+            //Change cell font
+            foreach (DataGridViewColumn c in dtgridView.Columns)
+            {
+                c.DefaultCellStyle.Font = new Font("Arial", 10F, GraphicsUnit.Pixel);
+            }
+        }
+        //from what will be copy
+        private void dtGrVwSymb_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            dtGrVwSymb.DoDragDrop(dtGrVwSymb.Rows
+            [e.RowIndex].Cells[e.ColumnIndex].Value.ToString(),
+            DragDropEffects.Copy);
+
+            // refresh the controll
+        }
+
+        private void dtgridView_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void dtgridView_DragDrop(object sender, DragEventArgs e)
+        {
+            int i, j;
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                bool pr = false;
+                // get value
+                var value = (System.String)e.Data.GetData(typeof(System.String));
+                // get coordinat's of mouse
+                Point clientPoint = dtgridView.PointToClient(new Point(e.X, e.Y));
+                // get the RowIndex
+                i = dtgridView.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+                // get the ColumnIndex
+                j = dtgridView.HitTest(clientPoint.X, clientPoint.Y).ColumnIndex;
+                if (dtgridView.Rows[i].Cells[j].Value.Equals(""))
+                {
+                    //check the rows
+                    for (int m = 0; m < check[1].Length; m++)
+                        if (j == check[1][m])
+                            if (Fix(i, m))
+                            {
+                                // displays the new value
+                                dtgridView.Rows[i].Cells[j].Value = value;
+                                pr = true;
+                                MessageBox.Show("Drag'n'drop was successfully end");
+                                AddCheck(i, j);
+                                break;
+                            }
+                    if (!pr)
+                        MessageBox.Show("You must drag the symbol near main word!");
+                    return;
+                }
+                else return;
+            }
+        }
+        private void AddCheck(int i, int j)
+        {
+            var m = check[check.Length - 2].Length;
+            var n = check[check.Length - 1].Length;
+            Array.Resize(ref check[check.Length - 2], m + 1);
+            Array.Resize(ref check[check.Length - 1], n + 1);
+            check[0][m] = i;
+            check[1][n] = j;
+        }
+        private bool Fix(int i, int m)
+        {
+            if ((i + 1) == check[0][m] || (i - 1) == check[0][m])
+                return true;
+            else return false;
+        }
+
+        private void MnItmExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
